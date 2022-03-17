8f2187b39a8fddbc16ffe4061b1577dfc0001806
Kleissner <peter@kleissner.investments>
2022-03-17 03:48:59 +0100

    Move code block to check the command line parameter immediately

--- Main.go
+++ Main.go
@@ -33,6 +33,16 @@ var config struct {
 }
 
 func main() {
+	apiListen, apiKey, watchPID := parseCmdParams()
+
+	// Exit if the API is not specified via command-line and the config file.
+	// This is the backend and an API must be provided for a frontend.
+	// This behavior makes sure that if the user accidentally starts this executable, it will do nothing.
+	if len(apiListen) == 0 && len(config.APIListen) == 0 {
+		os.Exit(core.ExitParamWebapiInvalid)
+		return
+	}
+
 	userAgent := appName + "/" + core.Version
 
 	filters := &core.Filters{
@@ -65,16 +75,6 @@ func main() {
 	}
 
 	backend.Stdout.Subscribe(os.Stdout)
-
-	apiListen, apiKey, watchPID := parseCmdParams()
-
-	// Exit if the API is not specified via command-line and the config file.
-	// This is the backend and an API must be provided for a frontend.
-	// This behavior makes sure that if the user accidentally starts this executable, it will do nothing.
-	if len(apiListen) == 0 && len(config.APIListen) == 0 {
-		os.Exit(core.ExitParamWebapiInvalid)
-		return
-	}
 
 	startAPI(backend, apiListen, apiKey)
 
